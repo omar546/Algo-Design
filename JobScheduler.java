20667c730666f4e3bddd0a1f080ce3c27d250cce
omar546 <omer.mkawy.om@gmail.com>
2023-05-15 20:46:03 +0200

    mek

--- JobScheduler.java
+++ JobScheduler.java
@@ -2,4 +2,11 @@ public class JobScheduler{
 	public static void main(String[] args) {
 		
 	}
+private record Job(int start_time, int end_time, int profit) implements Comparable<Job> {
+        @Override
+            public int compareTo(Job o) {
+                return this.end_time - o.end_time;
+            }
+        }
 }
+
