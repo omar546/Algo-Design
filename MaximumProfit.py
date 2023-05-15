b6a5812a857314ed03d8bff6715e742ebfe85425
Abdelrhmansalem777 <131315319+Abdelrhmansalem777@users.noreply.github.com>
2023-05-15 21:53:11 +0300

    Update MaximumProfit.py
    Find a free slot for each job (abdelrhmansalem

--- MaximumProfit.py
+++ MaximumProfit.py
@@ -12,3 +12,12 @@ def JobScheduling(arr):
         for j in range(n-1-i):
             if arr[j][2] < arr[j+1][2]:
                 arr[j],arr[j+1]=arr[j+1],arr[j]
+    #Find a free slot for each job
+    for i in range(n):
+        for j in range(min(t-1,arr[i][1]-1),-1,-1):
+            if job_id[j] == -1:
+                job_id[j] = arr[i][0]
+                job_profit[j] = arr[i][2]
+                break
+    print(len(job_id))
+    print(sum(job_profit)) 
